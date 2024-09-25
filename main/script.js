01e1bfe3fe3558f012deae613ab0d7f81ba9f6c1
Bhankid <100168498+Bhankid@users.noreply.github.com>
2024-09-25 10:02:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ main/script.js
@@ -0,0 +1,63 @@
+// const btnEl = document.querySelector(".btn");
+
+// btnEl.addEventListener("mouseover", (event) => {
+//   const x = event.offsetX;
+//   const y = event.offsetY;
+
+//   btnEl.style.setProperty("--xPos", x + "px");
+//   btnEl.style.setProperty("--yPos", y + "px");
+// });
+
+
+// Mobile Nav
+document.getElementById("open-nav").addEventListener("click", function (event) {
+  event.preventDefault();
+  document.querySelector(".nav-toggle").classList.toggle("active");
+});
+
+document
+  .getElementById("close-nav")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector(".nav-toggle").classList.toggle("active");
+  });
+
+document.addEventListener("click", function (event) {
+  if (!event.target.closest(".nav-toggle, .nav-links")) {
+    document.querySelector(".nav-toggle").classList.remove("active");
+  }
+});
+
+// Card swipper
+const stack = document.querySelector(".stack");
+const cards = Array.from(stack.children)
+  .reverse()
+  .filter((child) => child.classList.contains("card"));
+
+cards.forEach((card) => stack.appendChild(card));
+
+function moveCard() {
+  const lastCard = stack.lastElementChild;
+  if (lastCard.classList.contains("card")) {
+    lastCard.classList.add("swap");
+
+    setTimeout(() => {
+      lastCard.classList.remove("swap");
+      stack.insertBefore(lastCard, stack.firstElementChild);
+    }, 1200);
+  }
+}
+
+let autoplayInterval = setInterval(moveCard, 4000);
+
+stack.addEventListener("click", function (e) {
+  const card = e.target.closest(".card");
+  if (card && card === stack.lastElementChild) {
+    card.classList.add("swap");
+
+    setTimeout(() => {
+      card.classList.remove("swap");
+      stack.insertBefore(card, stack.firstElementChild);
+    }, 1200);
+  }
+});
